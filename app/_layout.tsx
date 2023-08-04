79ca25dbe21adfd3cce4912cde119164d9970214
bryanltobing <bryantobing0@gmail.com>
2023-08-04 22:27:55 +0800

    feat: add illustration,text,layout for home screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen } from 'expo-router';
 import { Stack } from 'expo-router/stack';
 import { useEffect } from 'react';
+import { useColorScheme } from 'react-native';
 
 import { colors } from '@/theme/colors';
 
@@ -11,6 +13,8 @@ export default function RootLayout() {
     'Mulish-SemiBold': require('../assets/fonts/Mulish-SemiBold.ttf'),
     'Mulish-Bold': require('../assets/fonts/Mulish-Bold.ttf'),
   });
+
+  const colorScheme = useColorScheme();
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   // TODO: handle this
@@ -29,16 +33,26 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.palette.brandColorDefault,
-        },
-        headerTintColor: colors.palette.neutralOffWhite,
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}
-    />
+    <ThemeProvider
+      value={
+        colorScheme === 'dark'
+          ? DarkTheme
+          : {
+              ...DefaultTheme,
+              colors: { ...DefaultTheme.colors, background: colors.palette.neutralWhite },
+            }
+      }>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: colors.palette.brandColorDefault,
+          },
+          headerTintColor: colors.palette.neutralOffWhite,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </ThemeProvider>
   );
 }
