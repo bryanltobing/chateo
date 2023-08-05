14b7fbfcbef36919fd09ac9e84f6068a1500368e
bryanltobing <bryantobing0@gmail.com>
2023-08-05 16:50:34 +0800

    feat: add ListItem component

---
+++ src/components/ui/Icons.tsx
@@ -0,0 +1,31 @@
+import { createIconSet } from '@expo/vector-icons';
+
+import { colors } from '@/theme/colors';
+
+const assets = require('../../../assets/icons/coolicons.ttf');
+
+/**
+ * References
+ * https://github.com/krystonschwarze/coolicons/issues/7#issuecomment-1645491673
+ */
+const glyphMap = {
+  Chevron_Down: 59772,
+  Chevron_Left: 59775,
+  Chevron_Right: 59778,
+  User_01: 60053,
+  Chat_Circle: 59757,
+  Mail: 59926,
+  Phone: 59969,
+};
+
+const CoolIcons = createIconSet(glyphMap, 'coolicons', assets);
+
+export type IconsProps = object & React.ComponentProps<typeof CoolIcons>;
+
+export const Icons = ({
+  color = colors.palette.neutralActive,
+  size = 24,
+  ...props
+}: IconsProps) => {
+  return <CoolIcons color={color} size={size} {...props} />;
+};
