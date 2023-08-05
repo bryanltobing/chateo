f29ac8a2f570a989404cb1f9e44477411a2ce258
bryanltobing <bryantobing0@gmail.com>
2023-08-05 18:14:09 +0800

    fix: add forwardRef support to ListItem component

--- src/components/ui/ListItem.tsx
+++ src/components/ui/ListItem.tsx
@@ -1,7 +1,9 @@
+import { Ref, forwardRef } from 'react';
 import { Pressable, PressableProps, StyleProp, View, ViewStyle } from 'react-native';
 
 import { Icons, IconsProps } from './Icons';
 import { Text } from './Text';
+
 import { colors } from '@/theme/colors';
 
 type ListItemProps = {
@@ -20,29 +22,32 @@ type ListItemProps = {
   hSpace?: boolean;
 } & PressableProps;
 
-export const ListItem = ({ text, icon, hSpace = false, ...props }: ListItemProps) => {
-  return (
-    <Pressable
-      {...props}
-      style={({ pressed, hovered }) => [
-        {
-          flexDirection: 'row',
-          paddingVertical: 8,
-          alignItems: 'center',
-          paddingHorizontal: hSpace ? 16 : 0,
-        },
-        (!!pressed || !!hovered) && pressedView,
-      ]}
-      android_ripple={{ color: colors.palette.neutralWeak }}
-    >
-      <View style={{ flexDirection: 'row', flex: 1, gap: 6, alignItems: 'center' }}>
-        {icon && <Icons name={icon} />}
-        <Text>{text}</Text>
-      </View>
-      {props.onPress && <Icons name="Chevron_Right" />}
-    </Pressable>
-  );
-};
+export const ListItem = forwardRef(
+  ({ text, icon, hSpace = false, ...props }: ListItemProps, ref: Ref<View>) => {
+    return (
+      <Pressable
+        {...props}
+        style={({ pressed, hovered }) => [
+          {
+            flexDirection: 'row',
+            paddingVertical: 8,
+            alignItems: 'center',
+            paddingHorizontal: hSpace ? 16 : 0,
+          },
+          (!!pressed || !!hovered) && pressedView,
+        ]}
+        android_ripple={{ color: colors.palette.neutralWeak }}
+        ref={ref}
+      >
+        <View style={{ flexDirection: 'row', flex: 1, gap: 6, alignItems: 'center' }}>
+          {icon && <Icons name={icon} />}
+          <Text>{text}</Text>
+        </View>
+        {props.onPress && <Icons name="Chevron_Right" />}
+      </Pressable>
+    );
+  }
+);
 
 const pressedView: StyleProp<ViewStyle> = {
   opacity: 0.5,
