67a4ee8f8485962feaf039949f1613b5c08ca549
bryanltobing <bryantobing0@gmail.com>
2023-08-06 18:44:09 +0800

    fix: keyboard avoiding view offset since we have removed headerTransparent true

--- src/components/KeyboardAvoidingView.tsx
+++ src/components/KeyboardAvoidingView.tsx
@@ -1,3 +1,4 @@
+import { useHeaderHeight } from '@react-navigation/elements';
 import {
   Platform,
   KeyboardAvoidingView as RnKeyboardAvoidingView,
@@ -10,5 +11,7 @@ export type KeyboardAvoidingViewProps = ViewProps;
 export const KeyboardAvoidingView = (props: KeyboardAvoidingViewProps) => {
   const Wrapper = Platform.OS === 'ios' ? RnKeyboardAvoidingView : View;
 
-  return <Wrapper behavior="padding" {...props} />;
+  const headerHeight = useHeaderHeight();
+
+  return <Wrapper behavior="padding" keyboardVerticalOffset={headerHeight} {...props} />;
 };
