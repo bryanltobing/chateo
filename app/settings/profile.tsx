c13b9abb657f33ed7697ca0c71754e937d89326d
bryanltobing <bryantobing0@gmail.com>
2023-08-09 22:50:39 +0800

    feat: add reanimated and fix keyboard avoiding view

--- app/settings/profile.tsx
+++ app/settings/profile.tsx
@@ -14,7 +14,7 @@ export default function ProfileSettingsPage() {
     <>
       <Stack.Screen options={{ title: 'Your Profile' }} />
 
-      <KeyboardAvoidingView>
+      <KeyboardAvoidingView style={{ gap: 16 }}>
         <ScrollView bounces={false} contentContainerStyle={{ paddingHorizontal: 24 }}>
           <View
             style={{ marginLeft: 'auto', marginRight: 'auto', paddingTop: 46, paddingBottom: 32 }}
--- src/components/KeyboardAvoidingView.tsx
+++ src/components/KeyboardAvoidingView.tsx
@@ -1,28 +1,20 @@
-import { Platform, View, ViewProps, useWindowDimensions } from 'react-native';
+import { ViewProps } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
-import { useFixedHeaderHeight } from '@/hooks/useFixedHeaderHeight';
 import { useKeyboardHeight } from '@/hooks/useKeyboardHeight';
 
 export type KeyboardAvoidingViewProps = ViewProps;
 
 export const KeyboardAvoidingView = ({ style, ...props }: KeyboardAvoidingViewProps) => {
-  const headerHeight = useFixedHeaderHeight();
   const keyboardHeight = useKeyboardHeight();
-  const windowDimensions = useWindowDimensions();
 
-  return (
-    <View
-      style={[
-        {
-          height:
-            Platform.OS === 'ios'
-              ? windowDimensions.height - keyboardHeight - headerHeight
-              : 'auto',
-          flex: Platform.OS === 'ios' ? undefined : 1,
-        },
-        style,
-      ]}
-      {...props}
-    />
-  );
+  const paddingBottomValue = useSharedValue(0);
+
+  const paddingBottomAnimatedStyle = useAnimatedStyle(() => ({
+    paddingBottom: withTiming(paddingBottomValue.value + keyboardHeight, {
+      duration: 230,
+    }),
+  }));
+
+  return <Animated.View style={[{ flex: 1 }, paddingBottomAnimatedStyle, style]} {...props} />;
 };
